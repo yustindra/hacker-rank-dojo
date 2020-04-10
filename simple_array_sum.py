0856b0462245d1d653c43eb167c2fd0da7328303
Yustindra <31244806+yustindra@users.noreply.github.com>
2020-04-10 20:47:46 +0900

    Create simple_array_sum.py

---
+++ simple_array_sum.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+def simpleArraySum(ar):
+    sums = 0
+    for i in ar:
+        sums += i
+    return sums
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
