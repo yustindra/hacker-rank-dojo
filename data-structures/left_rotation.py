c7da13b918708fc330f4bfe78427caab71dc2cdf
Yustindra <31244806+yustindra@users.noreply.github.com>
2020-04-22 21:11:01 +0900

    Create left_rotation.py

---
+++ data-structures/left_rotation.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    for i in range(d):
+        a = a[1:] + [a[0]]
+    print (*a)
