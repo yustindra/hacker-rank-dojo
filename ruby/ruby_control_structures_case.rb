458aeb55088911800bb918c1fd66b6ee00f62b57
Yustindra <31244806+yustindra@users.noreply.github.com>
2020-05-15 21:42:40 +0900

    Update ruby_control_structures_case.rb

--- ruby/ruby_control_structures_case.rb
+++ ruby/ruby_control_structures_case.rb
@@ -1,8 +1,8 @@
 def identify_class(obj)
     case obj
-        when Hacker, Submission, TestCase, Contest
+    when Hacker, Submission, TestCase, Contest
         puts "It's a #{obj.class}!"
-        else
+    else
         puts "It's an unknown model!"
     end
 end
