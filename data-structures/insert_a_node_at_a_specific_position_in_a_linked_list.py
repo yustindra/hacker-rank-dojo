a7c442db23139787ce1c533a07e54c9cf0441a6c
Yustindra <31244806+yustindra@users.noreply.github.com>
2020-05-21 21:44:26 +0900

    Create insert_a_node_at_a_specific_position_in_a_linked_list.py

---
+++ data-structures/insert_a_node_at_a_specific_position_in_a_linked_list.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def insertNodeAtPosition(head, data, position):
+    n = head
+    for _ in range(position - 1):
+        n = n.next
+        n_next = n.next
+    n.next = SinglyLinkedListNode(data)
+    n.next.next = n_next
+    return head
+
+if __name__ == '__main__':
