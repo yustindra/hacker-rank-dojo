21fb27f5bb227682cc5a42689c43c595e684e2d6
Yustindra <31244806+yustindra@users.noreply.github.com>
2020-05-25 21:58:37 +0900

    Create hash_tables_ransom_note.py

---
+++ interview-preparation-kit/hash_tables_ransom_note.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+def checkMagazine(magazine, note):
+    if Counter(note) - Counter(magazine) == Counter():
+        print ('Yes')
+    else:
+        print ('No')
+
+if __name__ == '__main__':
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    magazine = input().rstrip().split()
+
+    note = input().rstrip().split()
+
+    checkMagazine(magazine, note)
