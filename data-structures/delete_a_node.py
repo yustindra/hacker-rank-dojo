287b7195eadf270bc026415419cde7cc60f627f8
Yustindra <31244806+yustindra@users.noreply.github.com>
2020-06-10 21:59:46 +0900

    Create delete_a_node.py

---
+++ data-structures/delete_a_node.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def deleteNode(head, position):
+    if position == 0:
+        return head.next
+    else:
+        head.next = deleteNode(head.next, position - 1)
+        return head
+
+if __name__ == '__main__':
