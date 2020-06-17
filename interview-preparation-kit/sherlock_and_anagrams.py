3f603d2cd38d190daf5821f1285fa86b05d12241
Yustindra <31244806+yustindra@users.noreply.github.com>
2020-06-17 20:38:46 +0900

    Create sherlock_and_anagrams.py

---
+++ interview-preparation-kit/sherlock_and_anagrams.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+def sherlockAndAnagrams(s):
+    count = 0
+    for i in range(1, len(s)+1):
+        a = ["".join(sorted(s[j:j+i])) for j in range(len(s)-i+1)]
+        b = Counter(a)
+        for k in b:
+            count += b[k]*(b[k]-1)/2
+    return int(count)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = sherlockAndAnagrams(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
