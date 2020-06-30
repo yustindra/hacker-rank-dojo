fefc0c2bd9d8bff824338a23cc9ec16331ec68ca
Yustindra <31244806+yustindra@users.noreply.github.com>
2020-06-30 10:04:11 +0900

    Update ruby_enumerable_each_with_index.rb

--- ruby/ruby_enumerable_each_with_index.rb
+++ ruby/ruby_enumerable_each_with_index.rb
@@ -1,9 +1,5 @@
 def skip_animals(animals, skip)
     arr = []
-    animals.each_with_index do |item, index|
-        if index >= skip
-            arr.push "#{index}:#{item}"
-        end
-    end
+    animals.each_with_index {|item, index| arr << "#{index}:#{item}" if index >= skip}
     return arr
 end
